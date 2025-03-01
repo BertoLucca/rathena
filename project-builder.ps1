f995a496e16e9e4a1e5b8e2c3085aa2974a77f83
BertoLucca <BertoLucca@users.noreply.github.com>
2025-03-01 01:43:19 -0300

    pipeline: compilation fix

--- project-builder.ps1
+++ project-builder.ps1
@@ -1,14 +1,20 @@
 $rAthena = Resolve-Path "$PSScriptRoot";
 
-if (Test-Path "$rAthena/build/*") {
+Write-Host "Stating build proccess.";
+
+if (Test-Path "$rAthena/build") {
     Remove-Item "$rAthena/build/*" -verbose -Force -Recurse;
+} else {
+    New-Item -ItemType Directory -Path $rAthena -Name "build"
 }
 
 $( $images = docker image inspect rag-msvc-compiler:latest | ConvertFrom-Json ) 2>&1 | Out-Null;
 if ($images.length -eq 0) {
+    Write-Host "Image not found. Starting image build.";
     docker build -t rag-msvc-compiler:latest -f "$rAthena/Dockerfile" $rAthena;
 }
 
+Write-Host "Starting compilation.";
 docker run --rm `
     --entrypoint /rag/build.bat `
     --mount "type=bind,source=$rAthena,target=C:/rag-ro,readonly" `
